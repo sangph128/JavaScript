d98e3bddfe243335cf7efadad09de522b7f7b1d6
Sang Pham <sang.pham.8.12.96@gmail.com>
2018-12-22 16:33:45 -0600

    something new

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//Sample funtion
+function Addition(num1,num2) {
+
+	num1 = parseInt(prompt('Enter your fucking number'));
+	num2 = parseInt(prompt('ANOTHER ONE!!!!'));
+	total = num2 + num1;
+
+	return total;
+}
+
+newTotal = Addition();
+alert(newTotal);
